a291e35a2dda3d7f67e009b5ece9f77060172463
Sofia Slobodskaya <soniakorolyok@gmail.com>
2025-11-24 17:06:00 +0200

    fixed model

--- src/models/post.model.js
+++ src/models/post.model.js
@@ -1,16 +1,16 @@
 import mongoose from 'mongoose';
 
 const postSchema = new mongoose.Schema({
-    // author: { type: String, required: true },
-    title: {type: String, required: true},
-    content: {type: String, required: true},
-    tags: {type: [String], default: []},
+    title: { type: String, required: true },
+    content: { type: String, required: true },
+    author: { type: String},
+    dateCreated: { type: Date, default: Date.now },
+    tags: { type: [String], default: [] },
+    likes: { type: Number, default: 0 },
+    comments: { type: [String], default: [] },
 }, {
-    versionKey: false,
-    timestamps: true
+    versionKey: false
 });
 
 const Post = mongoose.model('Post', postSchema);
-
 export default Post;
-
