1cb2adbf71674d0c09645ad7ae23a2b1084bc5f6
Sofia Slobodskaya <soniakorolyok@gmail.com>
2025-11-30 18:46:02 +0200

    hw edd's solution

--- src/repositories/post.repository.js
+++ src/repositories/post.repository.js
@@ -1,60 +1,46 @@
-import Post from "../models/post.model.js";
 
+import Post from '../models/post.model.js';
 
 class PostRepository {
-    async createPost (postData) {
+    async createPost(postData){
         const post = new Post(postData);
         return post.save();
     }
-    async findPostById (id) {
+
+    async findPostById(id) {
         return Post.findById(id);
-    };
-    async deletePost (id) {
+    }
+
+    async deletePost(id) {
         return Post.findByIdAndDelete(id);
     }
+
     async addLike(id) {
-        return Post.findByIdAndUpdate(id, { $inc: { likes: 1 } }, { new: true });
-    }
-    async getPostsByAuthor(author) {
-        return Post.find({ author });
-    }
-    async addComment(id, commenter, message) {
-        const comment = {
-            user: commenter,
-            message,
-            dateCreated: new Date(),
-            likes: 0
-        };
-
-        const post = await Post.findByIdAndUpdate(
-            id,
-            { $push: { comments: comment } },
-            { new: true }
-        );
-
-        return post;
-    }
-    async getPostByTag(tagsString) {
-        const tagsArray = tagsString.split(',').map(tag => tag.trim());
-        return Post.find({
-            tags: {
-                $in: tagsArray.map(tag => new RegExp('^' + tag.replace(/[.*+?^${}()|[\]\\]/g, '\\$&'), 'i'))
-            }
-        });
-    }
-    async getPostsByPeriod(dateFrom, dateTo) {
-        return Post.find({
-            dateCreated: {
-                $gte: new Date(dateFrom),
-                $lte: new Date(dateTo)
-            }
-        });
-    }
-    async updatePost(id, data) {
-        return Post.findByIdAndUpdate(id, data, { new: true });
+        return Post.findByIdAndUpdate(id, {$inc: {likes: 1}}, {new: true});
     }
 
+    async findPostByAuthor(author) {
+        return Post.find({author: new RegExp(`^${author}$`, 'i')});
+    }
 
+    async addComment(id, comment) {
+        return Post.findByIdAndUpdate(id, {$push: {comments: comment}}, {new: true});
+    }
+
+    async findPostsByTags(tags) {
+        const regexConditions = tags.map(tag => ({
+            tags: new RegExp(`^${tag}$`, 'i')
+        }));
+        return Post.find({$or: regexConditions});
+    }
+
+    async findPostsByPeriod(dateFrom, dateTo) {
+        return Post.find({dateCreated: {$gte: dateFrom, $lte: dateTo}});
+    }
+
+    async updatePost(id, updateData) {
+        return Post.findByIdAndUpdate(id, updateData, {new: true});
+    }
 }
 
 export default new PostRepository();
--- src/services/post.service.js
+++ src/services/post.service.js
@@ -1,21 +1,18 @@
+
 import postRepository from '../repositories/post.repository.js';
-//import {NotFoundError} from '../utils/errors.js';
 
 class PostService {
-    async createPost (author, data) {
-        return await postRepository.createPost( { ...data, author });
-    };
+    async createPost(author, data) {
+        return await postRepository.createPost({...data, author});
+    }
 
-
-
-    async getPostById (id){
+    async getPostById(id) {
         const post = await postRepository.findPostById(id);
-        if (!post) {
-            throw new Error(`Post with id ${id} not found`);
+        if(!post) {
+            throw new Error(`Post with id ${id} not found`)
         }
         return post;
-    };
-
+    }
 
     async addLike(postId) {
         const post = await postRepository.addLike(postId);
@@ -25,18 +22,13 @@ class PostService {
         return post;
     }
 
-
     async getPostsByAuthor(author) {
-        const posts = await postRepository.getPostsByAuthor(author);
-        if (!posts.length) {
-            throw new Error(`No posts found for author ${author}`);
-        }
-        return posts;
+        return await postRepository.findPostByAuthor(author);
     }
 
-
     async addComment(postId, commenter, message) {
-        const post = await postRepository.addComment(postId, commenter, message);
+        const comment = {user: commenter, message};
+        const post = await postRepository.addComment(postId, comment);
         if (!post) {
             throw new Error(`Post with id ${postId} not found`);
         }
@@ -45,38 +37,31 @@ class PostService {
 
     async deletePost(postId) {
         const post = await postRepository.deletePost(postId);
-        if (!post) {
-            throw new Error(`Post with id ${postId} not found`);
+        if(!post) {
+            throw new Error(`Post with id ${postId} not found`)
         }
         return post;
     }
 
-    async getPostByTag(tagsString) {
-        const posts = await postRepository.getPostByTag(tagsString);
-        if (!posts || posts.length === 0) {
-            throw new Error(`No posts found with tags: ${tagsString}`);
-        }
-        return posts;
+    async getPostsByTags(tagsString) {
+        const tags = tagsString.split(',').map(tag => tag.trim().toLowerCase());
+        return await postRepository.findPostsByTags(tags);
     }
-
 
     async getPostsByPeriod(dateFrom, dateTo) {
-        const posts = await postRepository.getPostsByPeriod(dateFrom, dateTo);
-        if (!posts || posts.length === 0) {
-            throw new Error(`No posts found between ${dateFrom} and ${dateTo}`);
-        }
-        return posts;
+        return await postRepository.findPostsByPeriod(new Date(dateFrom), new Date(dateTo));
     }
 
-
-    async updatePost(id, data) {
-        const updatedPost = await postRepository.updatePost(id, data);
-        if (!updatedPost) {
-            throw new Error(`Post with id ${id} not found`);
+    async updatePost(postId, data) {
+        const post = await postRepository.findPostById(postId);
+        if (!post) {
+            throw new Error(`Post with id ${postId} not found`);
         }
-        return updatedPost;
+        if (data.tags) {
+            data.tags.push(...post.tags);
+        }
+        return await postRepository.updatePost(postId, data);
     }
-
 }
 
-export default new PostService();
+export default new PostService()
